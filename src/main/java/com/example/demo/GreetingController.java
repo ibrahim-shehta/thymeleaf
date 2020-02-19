bdaf7b760bd70a948b90e0ea008158535d8ddce8
ibrahim <ibrahim@ibrahim>
2020-02-19 11:26:51 +0200

    layout

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,34 +1,32 @@
 package com.example.demo;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.modules.user.model.User;
-import com.example.demo.modules.user.service.UserService;
 
 @Controller
 public class GreetingController {
-	
-	@Autowired
-	private UserService userServiceImpl;
-	
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
+
+
+	@GetMapping(value = {"/", "index"})
+	public String index() {
+		return "index";
 	}
-	
-	@GetMapping("/greeting2")
-	public String greeting2(Model model) {
+
+	@GetMapping("/register")
+	public String register(Model model) {
 		
-		 Optional<User> user =  userServiceImpl.findById(1);
-		model.addAttribute("name", user.get().getUsername());
-		return "greeting";
+		return "register";
 	}
+	
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		
+		
+		return "login";
+	}
+
 
 }
